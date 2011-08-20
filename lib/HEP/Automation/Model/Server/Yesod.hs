6749aaae207951197b129c47c60ad96100247e25
ianwookim <ianwookim@gmail.com>
2011-08-20 01:00:40 +0000

    implement CRUD operations

--- lib/HEP/Automation/Model/Server/Yesod.hs
+++ lib/HEP/Automation/Model/Server/Yesod.hs
@@ -10,8 +10,20 @@ module HEP.Automation.Model.Server.Yesod where
 
 import Yesod hiding (update)
 
+import Network.Wai
+import Network.Wai.Parse
+
+import qualified Data.Enumerator as E
+import qualified Data.ByteString as S
+import qualified Data.ByteString.Char8 as SC
+
 import HEP.Automation.Model.Type
 import Data.Acid
+
+import Data.Attoparsec as P
+import Data.Aeson as A
+import Data.Aeson.Parser
+import qualified Data.Aeson.Generic as G
 
 data ModelServer = ModelServer {
   server_acid :: AcidState ModelInfoRepository
@@ -19,7 +31,9 @@ data ModelServer = ModelServer {
 
 mkYesod "ModelServer" [parseRoutes|
 / HomeR GET
-/model/#String ModelR GET
+/listmodel  ListModelR GET
+/uploadmodel  UploadModelR POST
+/model/#String ModelR 
 |]
 
 instance Yesod ModelServer where
@@ -32,14 +46,97 @@ getHomeR = do
   liftIO $ putStrLn "getHomeR called"
   defaultLayout [hamlet|Hello World!|]
 
-getModelR :: String -> Handler RepHtml
+
+getListModelR :: Handler RepHtml
+getListModelR = undefined
+
+postUploadModelR :: Handler RepHtmlJson
+postUploadModelR = undefined
+
+handleModelR :: String -> Handler RepHtmlJson
+handleModelR name = do
+  wr <- return.reqWaiRequest =<< getRequest
+  case requestMethod wr of 
+    "GET" -> getModelR name
+    "PUT" -> putModelR name
+    "DELETE" -> deleteModelR name
+
+getModelR :: String -> Handler RepHtmlJson
 getModelR modelname = do 
+  liftIO $ putStrLn "getModelR called"
   acid <- return.server_acid =<< getYesod
   r <- liftIO $ query acid (QueryModel modelname)
   liftIO $ putStrLn $ show r 
+  defaultLayoutJson [hamlet| <h1> File #{modelname}|] (G.toJSON r)
 
-  defaultLayout [hamlet| <h1> File #{modelname}
-|]
+putModelR :: String -> Handler RepHtmlJson
+putModelR modelname = do 
+  liftIO $ putStrLn "putModelR called"
+  acid <- return.server_acid =<< getYesod
+  wr <- return.reqWaiRequest =<< getRequest
+  bs' <- lift E.consume
+  let bs = S.concat bs'
+  let parsed = parse json bs 
+  case parsed of 
+    Done _ parsedjson -> do 
+      case (G.fromJSON parsedjson :: A.Result ModelInfo) of 
+        Success minfo -> do 
+          if modelname == model_name minfo
+            then do r <- liftIO $ update acid (UpdateModel minfo)
+                    defaultLayoutJson [hamlet| <h1> File #{modelname}|] 
+                                      (G.toJSON r)
+            else do liftIO $ putStrLn "modelname mismatched"
+                    defaultLayoutJson [hamlet| <h1> File #{modelname}|] 
+                                      (G.toJSON (Nothing :: Maybe ModelInfo))
+        Error err -> do 
+          liftIO $ putStrLn err 
+          defaultLayoutJson [hamlet| <h1> File #{modelname}|] 
+                            (G.toJSON (Nothing :: Maybe ModelInfo))
+    Fail _ ctxts err -> do 
+      liftIO $ putStrLn (concat ctxts++err)
+      defaultLayoutJson [hamlet| <h1> File #{modelname}|] 
+                        (G.toJSON (Nothing :: Maybe ModelInfo))
+         
+    Partial _ -> do 
+      liftIO $ putStrLn "partial" 
+      defaultLayoutJson [hamlet| <h1> File #{modelname}|] 
+                        (G.toJSON (Nothing :: Maybe ModelInfo))
+
+deleteModelR :: String -> Handler RepHtmlJson
+deleteModelR modelname = do 
+  acid <- return.server_acid =<< getYesod
+  r <- liftIO $ update acid (DeleteModel modelname)
+  liftIO $ putStrLn $ show r 
+  defaultLayoutJson [hamlet| <h1> File #{modelname}|] (G.toJSON r)
+
+postModelUploadR :: Handler RepHtmlJson
+postModelUploadR = do 
+  liftIO $ putStrLn "postModelUpdateR called"
+  acid <- return.server_acid =<< getYesod
+  wr <- return.reqWaiRequest =<< getRequest
+  bs' <- lift E.consume
+  let bs = S.concat bs'
+  let parsed = parse json bs 
+  case parsed of 
+    Done _ parsedjson -> do 
+      case (G.fromJSON parsedjson :: A.Result ModelInfo) of 
+        Success minfo -> do 
+          r <- liftIO $ update acid (AddModel minfo)
+          defaultLayoutJson [hamlet| <h1> Posted|] 
+                            (G.toJSON r)
+        Error err -> do 
+          liftIO $ putStrLn err 
+          defaultLayoutJson [hamlet| <h1> Not posted|] 
+                            (G.toJSON (Nothing :: Maybe ModelInfo))
+    Fail _ ctxts err -> do 
+      liftIO $ putStrLn (concat ctxts++err)
+      defaultLayoutJson [hamlet| <h1> Not posted|] 
+                        (G.toJSON (Nothing :: Maybe ModelInfo))
+         
+    Partial _ -> do 
+      liftIO $ putStrLn "partial" 
+      defaultLayoutJson [hamlet| <h1> Not posted|] 
+                        (G.toJSON (Nothing :: Maybe ModelInfo))
 
 
 
